c41fd8f7c5c25eaac2ae624d53404aa9ea7f3f58
mrtn <martinakopyan34@gmail.com>
2025-06-17 19:53:51 +0400

    fix lessons migrations.

--- database/migrations/2025_06_17_081046_create_lessons_table.php
+++ database/migrations/2025_06_17_081046_create_lessons_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\LessonStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,9 +16,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('instructor_id')->constrained('users');
             $table->foreignId('student_id')->constrained('users');
-            $table->timetamp('start_time');
-            $table->timestamp('end_time');
-            $table->enum('status', ['planned', 'confirmed', 'completed', 'cancelled'])->default('planned');
+            $table->timestamp('start_time')->nullable();
+            $table->timestamp('end_time')->nullable();
+            $table->enum('status', ['planned', 'confirmed', 'completed', 'cancelled'])->default(LessonStatus::PLANNED);
             $table->text('notes')->nullable();
             $table->index(['instructor_id', 'start_time']);
             $table->timestamps();

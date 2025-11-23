7dadcae8ed7deab1d9308f280744a6704086a59c
mrtn <martinakopyan34@gmail.com>
2025-11-23 14:24:00 +0300

    Refactor ApproveInstructor.php mutation;

--- app/GraphQL/Mutations/ApproveInstructor.php
+++ app/GraphQL/Mutations/ApproveInstructor.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\GraphQL\Mutations;
 
+use App\Exceptions\InstructorApprovalException;
 use App\Models\User;
+use App\Services\InstructorApprovalService;
 use Closure;
 use GraphQL\Type\Definition\ResolveInfo;
 use GraphQL\Type\Definition\Type;
@@ -19,6 +21,10 @@ class ApproveInstructor extends Mutation
         'description' => 'Mutation to approve an instructor (admin only)'
     ];
 
+    public function __construct(
+        private readonly InstructorApprovalService $approvalService
+    ){}
+
     public function type(): Type
     {
         return GraphQL::type('User');
@@ -27,33 +33,21 @@ class ApproveInstructor extends Mutation
     public function args(): array
     {
         return [
-            'id' => [
+            'instructor_id' => [
                 'type' => Type::nonNull(Type::int()),
                 'description' => 'ID of instructor to approve'
             ]
         ];
     }
 
-    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
+    /**
+     * @throws InstructorApprovalException
+     */
+    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields): User
     {
 
         $admin = auth()->user();
 
-        if (!$admin || !$admin->isAdmin()) {
-            throw new \Exception('Unauthorized: Only admins can approve instructors.');
-        }
-
-
-        $instructor = User::findOrFail($args['id']);
-
-        if (!$instructor->isInstructor()) {
-            throw new \Exception('User is not an instructor.');
-        }
-
-        $instructor->update([
-            'is_approved' => true,
-        ]);
-
-        return $instructor;
+        return $this->approvalService->approveInstructor($args['instructor_id'], $admin);
     }
 }

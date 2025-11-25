12837a4504b0e341a92be00820706ea953466224
mrtn <martinakopyan34@gmail.com>
2025-11-25 11:34:02 +0300

    Add Events (LessonCreated, LessonConfirmed, LessonCancelled, LessonCancelledBySystem, InstructorApproved, InstructorRejected)

---
+++ app/Events/LessonCancelledBySystem.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Lesson;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LessonCancelledBySystem
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public Lesson $lesson,
+        public string $reason
+    ){}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}

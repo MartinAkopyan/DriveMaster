56a865f2b35333782323373ea166af0e920eb218
mrtn <martinakopyan34@gmail.com>
2025-12-01 11:33:27 +0300

    Add test cannot_book_with_unapproved_instructor.

--- tests/Feature/LessonBookingTest.php
+++ tests/Feature/LessonBookingTest.php
@@ -75,20 +75,46 @@ class LessonBookingTest extends TestCase
             ) {
                 id
             }
-        }
-    ";
+        }";
 
         $response = $this->actingAs($this->instructor, 'sanctum')
             ->postJson('/graphql', ['query' => $query]);
 
-        \Log::info('after mutation');
-
         $response->assertOk();
         $response->assertJsonPath('errors.0.message', 'Only students can book lessons');
 
-        \Log::info($response->json());
-        \Log::info('after response');
+        $this->assertDatabaseCount('lessons', 0);
+    }
 
+    /** @test */
+    public function cannot_book_with_unapproved_instructor(): void
+    {
+        $unApprovedInstructor = User::factory()->create([
+            'role' => UserRole::INSTRUCTOR,
+            'is_approved' => false
+        ]);
+
+        $date = now()->addDays(5)->format('Y-m-d');
+
+        $query = "
+            mutation{
+                bookLesson(
+                    instructor_id: {$unApprovedInstructor->id},
+                    date: \"{$date}\",
+                    slot: 1
+                ) {
+                    id
+                }
+            }
+        ";
+
+        $response = $this->actingAs($this->student, 'sanctum')
+            ->postJson('/graphql', ['query' => $query]);
+
+        \Log::info($response->json());
+
+        $response->assertOk();
+        $response->assertJsonPath('errors.0.message', 'Instructor not found or not approved');
         $this->assertDatabaseCount('lessons', 0);
     }
 }

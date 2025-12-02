2624a6385ee1b7f8ba0dbd670f935c2fcfc7c9e7
mrtn <martinakopyan34@gmail.com>
2025-12-02 16:23:44 +0300

    Add RejectInstructorTest: • student_cannot_reject_instructor test • instructor_cannot_reject_instructor test • cannot_reject_already_approved_instructor test • cannot_reject_non_instructor test

--- tests/Feature/GraphQL/Mutations/RejectInstructorTest.php
+++ tests/Feature/GraphQL/Mutations/RejectInstructorTest.php
@@ -58,6 +58,90 @@ class RejectInstructorTest extends TestCase
         $this->assertSoftDeleted('users', [
             'id' => $this->instructor->id,
         ]);
+    }
 
+    /** @test */
+    public function student_cannot_reject_instructor(): void
+    {
+        $student = User::factory()->create(['role' => UserRole::STUDENT]);
+
+        $response = $this->actingAs($student, 'sanctum')
+            ->postJson('/graphql', ['query' => $this->query]);
+
+        $response->assertOk();
+        $response->assertJsonPath('errors.0.message', 'Only admins can perform this action');
+        $this->assertDatabaseHas('users', [
+            'id' => $this->instructor->id,
+            'deleted_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function instructor_cannot_reject_instructor(): void
+    {
+        $anotherInstructor = User::factory()->create(['role' => UserRole::INSTRUCTOR]);
+        $response = $this->actingAs($anotherInstructor, 'sanctum')
+            ->postJson('graphql', ['query' => $this->query]);
+
+        $response->assertOk();
+        $response->assertJsonPath('errors.0.message', 'Only admins can perform this action');
+        $this->assertDatabaseHas('users', [
+            'id' => $this->instructor->id,
+            'deleted_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function cannot_reject_already_approved_instructor(): void
+    {
+        $anotherInstructor = User::factory()->create([
+            'role' => UserRole::INSTRUCTOR,
+            'is_approved' => true
+        ]);
+
+        $query2 = "mutation {
+                             rejectInstructor(
+                                instructor_id: {$anotherInstructor->id},
+                                reason: \"Insufficient experience\") {
+                                    id
+                                    profile {
+                                        rejection_reason
+                                    }
+                                }
+                             }";
+
+        $response = $this->actingAs($this->admin, 'sanctum')
+            ->postJson('/graphql', ['query' => $query2]);
+
+        $response->assertOk();
+        $response->assertJsonPath('errors.0.message', 'Cannot reject approved instructor');
+        $this->assertDatabaseHas('users', [
+           'id' => $anotherInstructor->id,
+           'deleted_at' => null,
+        ]);
+    }
+
+    /** @test */
+    public function cannot_reject_non_instructor(): void
+    {
+        $student = User::factory()->create(['role' => UserRole::STUDENT]);
+
+        $query2 = "mutation { rejectInstructor(
+                            instructor_id: {$student->id},
+                            reason: \"Insufficient experience\") {
+                                id
+                                profile {
+                                    rejection_reason
+                                }}}";
+
+        $response = $this->actingAs($this->admin, 'sanctum')
+            ->postJson('/graphql', ['query' => $query2]);
+
+        $response->assertOk();
+        $response->assertJsonPath('errors.0.message', 'Cannot find instructor');
+        $this->assertDatabaseHas('users', [
+            'id' => $student->id,
+            'deleted_at' => null,
+        ]);
     }
 }
